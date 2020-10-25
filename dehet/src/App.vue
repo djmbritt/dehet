19088b1bffa8cded328c978d7250c50388cccf6f
D.J.M. Britt <david.britt@hva.nl>
2020-10-25 17:19:44 +0100

    text box

--- dehet/src/App.vue
+++ dehet/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
-    <p>Hello</p>
+    
+    <h1>Hello</h1>
+
+    <form method="get" action="">
+      <div class="form-group">
+        <label for="checker">DeHet</label>
+        <input id="checker" class="form-control" type="text" name="">
+      </div>
+    </form>
+
   </div>
 </template>
