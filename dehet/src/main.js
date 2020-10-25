bd757e2eedce42ef4241a25bd05b64b4bf9dddcb
D.J.M. Britt <david.britt@hva.nl>
2020-10-25 16:52:54 +0100

    Vue created

--- dehet/src/main.js
+++ dehet/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
