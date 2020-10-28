905bf41ff0cab9905d3cd25cf4c902ed1b5839a2
D.J.M. Britt <david.britt@hva.nl>
2020-10-28 01:02:48 +0100

    Stashing to add veutify

--- dehet/src/App.vue
+++ dehet/src/App.vue
@@ -1,24 +1,25 @@
 <template>
-      <div>
-        <b-form-textarea
-          id="textarea"
-          v-model="text"
-          placeholder="Enter something..."
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
+  <div>
+  <h1>Text area.</h1>
+  <br>
+    <b-form-textarea
+      id="textarea"
+      v-model="text"
+      placeholder="What will happen."
+      rows="3"
+      max-rows="6"
+    ></b-form-textarea>
 
-        <pre class="mt-3 mb-0">{{ text }}</pre>
-      </div>      
-
+    <pre class="mt-3 mb-0">{{ text }}</pre>
+  </div>
 </template>
 
 <script>
-      export default {
-        data() {
-          return {
-            text: ''
-          }
-        }
-      }
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  }
+};
 </script>
--- dehet/src/main.js
+++ dehet/src/main.js
@@ -2,13 +2,15 @@ import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
+// import router from "./router";
+import veutify from "@/plugins/veutify";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  veutify,
   render: h => h(App)
 }).$mount("#app");
